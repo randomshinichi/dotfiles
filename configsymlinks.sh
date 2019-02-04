6af245e3580aa15937d6ff3910e4696ae33ff2a5
Andrew Chiw <randomshinichi4869@gmail.com>
2019-02-04 23:53:53 +0100

    removed nvidia, snapper. added configsymlinks.sh

---
+++ configsymlinks.sh
@@ -0,0 +1,7 @@
+ln -s ~/source/dotfiles/files/.emacs ~/.emacs
+ln -s ~/source/dotfiles/files/i3/config ~/.config/i3/config
+ln -s ~/source/dotfiles/files/i3/i3status.conf ~/.config/i3/i3status.conf
+ln -s ~/source/dotfiles/files/.bash_profile ~/.bash_profile
+
+ln -s ~/source/dotfiles/files/.gitconfig ~/.gitconfig
+ln -s ~/source/dotfiles/files/.xprofile ~/.xprofile
