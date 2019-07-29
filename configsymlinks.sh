c14adbe073c00437f131d374e29522f8becdbeee
Andrew Chiw <randomshinichi4869@gmail.com>
2019-07-30 00:59:51 +0200

    workspace misc, autorandr horizontal/vertical, newsboat upon login and i3 saved layout

--- configsymlinks.sh
+++ configsymlinks.sh
@@ -2,7 +2,7 @@ ln -s ~/source/dotfiles/files/.emacs ~/.emacs
 
 ln -s ~/source/dotfiles/files/i3/config ~/.config/i3/config
 ln -s ~/source/dotfiles/files/i3/i3status.conf ~/.config/i3/i3status.conf
-ln -s ~/source/dotfiles/files/i3/workspace-3.json ~/.config/i3/workspace-3.json
+ln -s ~/source/dotfiles/files/i3/workspace-misc.json ~/.config/i3/workspace-misc.json
 ln -s ~/source/dotfiles/files/i3/o.sh ~/.config/i3/o.sh
 ln -s ~/source/dotfiles/files/i3/eject.sh ~/.config/i3/eject.sh
 
