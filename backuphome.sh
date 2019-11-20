b9f01ee2003acfbd6c04d67ca2e2319756ca8c48
Andrew Chiw <randomshinichi4869@gmail.com>
2019-11-20 02:00:14 +0100

    update backuphome.sh

--- backuphome.sh
+++ backuphome.sh
@@ -1,2 +1,2 @@
 sudo tar cvjpf wifi.tbz2 /etc/NetworkManager/system-connections
-tar cvjpf home.tbz2 .env_secrets .ironsocket/ .config/autorandr .config/syncthing .config/chromium .config/Code .config/sublime-text-3 .config/skypeforlinux .minecraft/ .mozilla/ .quodlibet/ .ssh/  Documents/ source/ --exclude=source/go
+tar cvpf home.tar .env_secrets .ironsocket/ .config/kitty .config/ranger .config/syncthing .config/chromium .config/Code .config/sublime-text-3 .config/skypeforlinux .emacs.d/ .minecraft/ .mozilla/ .newsboat/ .quodlibet/ .ssh/  Documents/ source/
