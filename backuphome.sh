32e573898232c405ec2d3914a77ab5281d9c99a2
Andrew Chiw <randomshinichi4869@gmail.com>
2022-07-01 14:28:31 +0200

    backup more stuff

--- backuphome.sh
+++ backuphome.sh
@@ -1,2 +1,2 @@
 sudo tar cvjpf wifi.tbz2 /etc/NetworkManager/system-connections
-tar cvpf home.tar .env_secrets .ironsocket/ .config/kitty .config/ranger .config/syncthing .config/chromium .config/Code .config/sublime-text-3 .config/skypeforlinux .emacs.d/ .minecraft/ .mozilla/ .newsboat/ .quodlibet/ .ssh/  Documents/ source/
+tar cvpf home.tar .env_secrets .ironsocket/ .config/ranger .config/syncthing .config/chromium .config/sublime-text-3 .config/discord ".config/Code - OSS" .config/deemix .config/gzdoom .config/Exodus .emacs.d/ .mozilla/ .newsboat/ .quodlibet/ .thunderbird .ssh/ Documents/ source/
